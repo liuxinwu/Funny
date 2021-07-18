0c3ee7ae226b6a26399e559eb8f0351874a9d293
xinnwuliu <13242420347@163.com>
2021-07-18 19:09:37 +0800

    feat: 路由动画、视频详情页

---
+++ lib/router/index.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:funny/pages/app.dart';
+import 'package:funny/pages/video-detail/index.dart';
+import 'package:funny/pages/video-detail/playPage.dart';
+
+class FRoutes {
+  static final routes = <String, WidgetBuilder>{
+    '/': (BuildContext context) => App(),
+  };
+
+  static final routers = <String, dynamic>{
+    'videoDetail': (arguments) => VideoDetail(
+          routeArgs: arguments,
+        ),
+    'videoPlayPage': (arguments) => PlayPage(
+          routeArgs: arguments,
+        )
+  };
+
+  static final RouteFactory? onGenerateRoute = (settings) {
+    String name = settings.name.toString();
+    print(name);
+    bool isInRouters =
+        FRoutes.routers.keys.toList().indexOf(name, 0) < 0 ? false : true;
+    if (isInRouters)
+      return PageRouteBuilder(
+          pageBuilder: (context, animation, secondaryAnimation) =>
+              FRoutes.routers[name](settings.arguments),
+          transitionsBuilder: (context, animation, secondaryAnimation, child) =>
+              SlideTransition(
+                position: Tween<Offset>(
+                  begin: const Offset(1, 0),
+                  end: Offset.zero,
+                ).animate(animation),
+                child: child,
+              ));
+  };
+
+  static final RouteFactory? onUnknownRoute = (RouteSettings settings) {
+    return MaterialPageRoute(builder: (context) {
+      return Text('404');
+    });
+  };
+}
