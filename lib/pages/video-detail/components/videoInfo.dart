2c41fa96a0c5921ff541a89d8b12d6961a0ee154
xinnwuliu <13242420347@163.com>
2021-08-01 14:40:19 +0800

    删除不需要代码、注释

--- lib/pages/video-detail/components/videoInfo.dart
+++ lib/pages/video-detail/components/videoInfo.dart
@@ -1,49 +1,201 @@
 import 'package:flutter/material.dart';
+import 'package:funny/utils/iconfont.dart';
 
-class VideoInfo extends StatelessWidget {
-  VideoInfo({required this.data});
+class PlayVideoInfo extends StatelessWidget {
+  PlayVideoInfo({required this.data});
 
   final Map data;
 
-  TextStyle getStyle() {
-    return TextStyle(fontSize: 14, height: 1.5);
+  // 简介信息
+  _showModalBottomSheet(BuildContext context) {
+    final vodTotal = data['vodTotal'] ?? 0;
+    final vodArea = data['vodArea'] ?? 0;
+    final vodYear = data['vodYear'] ?? 0;
+    final vodClass = data['vodClass']?.replaceAll(RegExp(r','), '/') ?? '';
+    final List vodActor = data['vodActor']?.split(',') ?? '';
+
+    return showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          padding: EdgeInsets.all(16),
+          child: Scrollbar(
+            child: SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    data['vodName'] ?? '',
+                    style: TextStyle(
+                        fontSize: 16, fontWeight: FontWeight.bold, height: 2),
+                  ),
+                  Text(
+                    '全 $vodTotal 集',
+                    style: TextStyle(
+                        color: Color.fromRGBO(132, 132, 148, 1),
+                        fontSize: 12,
+                        height: 2),
+                  ),
+                  Text(
+                    '$vodArea $vodYear $vodClass',
+                    style: TextStyle(
+                        color: Color.fromRGBO(132, 132, 148, 1),
+                        fontSize: 12,
+                        height: 2),
+                  ),
+                  Scrollbar(
+                      child: SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          child: Row(
+                            children: vodActor
+                                .map((actor) => Column(
+                                      children: [
+                                        ClipRRect(
+                                          borderRadius:
+                                              BorderRadius.circular(70),
+                                          child: Image.asset(
+                                              'lib/images/logo.png',
+                                              width: 70,
+                                              centerSlice: Rect.largest),
+                                        ),
+                                        Text(actor,
+                                            style: TextStyle(
+                                                color: Color.fromRGBO(
+                                                    132, 132, 148, 1),
+                                                fontSize: 12,
+                                                height: 1.5))
+                                      ],
+                                    ))
+                                .toList(),
+                          ))),
+                  Text(
+                    '简介',
+                    style: TextStyle(
+                        fontSize: 16, fontWeight: FontWeight.bold, height: 2),
+                  ),
+                  Text(
+                    data['vodContent'] ?? '',
+                    style: TextStyle(
+                        color: Color.fromRGBO(132, 132, 148, 1),
+                        fontSize: 12,
+                        height: 2),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return IntrinsicHeight(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          (data['vod_pic'] != null)
-              ? Image.network(
-                  data['vod_pic'],
-                  width: 180,
-                )
-              : Image.asset('lib/images/logo.png', width: 180),
-          SizedBox(
-            width: 10,
-          ),
-          Expanded(
-              flex: 1,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    data['vod_name'] ?? '',
+    final vodScore = data['vodScore'] ?? 0;
+    final vodTotal = data['vodTotal'] ?? 0;
+    final vodHits = data['vodHits'] ?? 0;
+    final updateCollection = data['vodPlayUrl']?.length ?? 0;
+    final vodUp = data['vodUp'] ?? 0;
+    final primaryColor = Theme.of(context).primaryColor;
+
+    return ListBody(
+      children: [
+        Text(
+          data['vodName'] ?? '',
+          style:
+              TextStyle(fontSize: 18, fontWeight: FontWeight.bold, height: 1.5),
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 5, bottom: 5),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                '$vodScore 分',
+                style: TextStyle(
+                    color: Color.fromRGBO(132, 132, 148, 1),
+                    fontSize: 12,
+                    height: 1.5),
+              ),
+              Text(
+                ' · ',
+                style: TextStyle(
+                  color: Color.fromRGBO(132, 132, 148, 1),
+                  fontSize: 12,
+                  height: 1.5,
+                ),
+              ),
+              Text(
+                'VIP',
+                style:
+                    TextStyle(color: primaryColor, fontSize: 12, height: 1.5),
+              ),
+              Text(
+                ' · 更新至$updateCollection集 · 全$vodTotal集 · $vodHits次播放',
+                style: TextStyle(
+                    color: Color.fromRGBO(132, 132, 148, 1),
+                    fontSize: 12,
+                    height: 1.5),
+              ),
+              GestureDetector(
+                onTap: () async {
+                  _showModalBottomSheet(context);
+                },
+                child: Text(' · 简介 >',
                     style: TextStyle(
-                        fontSize: 16, fontWeight: FontWeight.bold, height: 2),
+                        color: Colors.blue, fontSize: 12, height: 1.5)),
+              )
+            ],
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 5, bottom: 5),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Wrap(
+                crossAxisAlignment: WrapCrossAlignment.center,
+                children: [
+                  Icon(
+                    FIcons.likes,
+                    color: Color.fromRGBO(34, 34, 34, 1),
                   ),
-                  Text('主演：${data['vod_actor']}', style: getStyle()),
-                  Text('导演：${data['vod_director']}', style: getStyle()),
-                  Text('地区：${data['vod_area']}', style: getStyle()),
-                  Text('时间：${data['vod_time']}', style: getStyle()),
-                  Text('类型：${data['vod_class']}', style: getStyle()),
-                  Text('语言：${data['vod_lang']}', style: getStyle())
+                  Text(
+                    ' $vodUp+',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 12,
+                      height: 1.5,
+                    ),
+                  )
                 ],
-              ))
-        ],
-      ),
+              ),
+              Wrap(
+                spacing: 10,
+                crossAxisAlignment: WrapCrossAlignment.center,
+                children: [
+                  Icon(
+                    FIcons.collection,
+                    size: 18,
+                    color: Color.fromRGBO(34, 34, 34, 1),
+                  ),
+                  Icon(
+                    FIcons.download,
+                    size: 24,
+                    color: Color.fromRGBO(34, 34, 34, 1),
+                  ),
+                  Icon(
+                    FIcons.share,
+                    color: Color.fromRGBO(34, 34, 34, 1),
+                  ),
+                ],
+              )
+            ],
+          ),
+        )
+      ],
     );
   }
 }
--- lib/pages/video-detail/index.dart
+++ lib/pages/video-detail/index.dart
@@ -1,38 +1,69 @@
 import 'package:flutter/material.dart';
 import 'package:funny/api/cmsApi.dart';
-import 'package:funny/pages/video-detail/components/videoCollection.dart';
-import 'package:funny/pages/video-detail/components/videoComment.dart';
-import 'package:funny/pages/video-detail/components/videoDesc.dart';
-import 'package:funny/pages/video-detail/components/videoInfo.dart';
+import 'package:funny/components/fHorizontalVideo.dart';
+import 'package:funny/components/fNav.dart';
+import 'package:funny/components/fPlayer.dart';
+import 'package:funny/utils/evnet.dart';
 
-class VideoDetail extends StatefulWidget {
-  VideoDetail({Key? key, this.routeArgs}) : super(key: key);
+import 'components/playVideoInfo.dart';
+import 'components/videoCollection.dart';
+import 'components/videoComment.dart';
+
+class PlayPage extends StatefulWidget {
+  PlayPage({Key? key, this.routeArgs}) : super(key: key);
 
   final routeArgs;
 
   @override
-  _VideoDetailState createState() => _VideoDetailState(routeArgs: routeArgs);
+  _PlayPage createState() => _PlayPage(routeArgs: routeArgs);
 }
 
-class _VideoDetailState extends State {
-  _VideoDetailState({this.routeArgs}) : super();
+class _PlayPage extends State<PlayPage> {
+  _PlayPage({this.routeArgs});
 
   final routeArgs;
-  Map videoDetail = {};
+  Map videoInfo = {};
+  // 推荐列表
+  List recommendList = [];
+  // 热播视频
+  List hotList = [];
 
-  getVideoDetail() async {
+  getVideoInfo() async {
     print(routeArgs);
     final res =
         await CmsApi.getDetail(queryParameters: {'ids': routeArgs['videoId']});
-    print(res.data.length);
+    final data = res.data[0] ?? [];
+
     setState(() {
-      videoDetail = res.data[0];
+      videoInfo = data;
+    });
+  }
+
+  getRecommendList() async {
+    final res =
+        await CmsApi.getList(queryParameters: {'ids': videoInfo['typeId']});
+    setState(() {
+      recommendList = res.data;
+    });
+  }
+
+  getHotList() async {
+    final res = await CmsApi.getHotList();
+    setState(() {
+      hotList = res.data;
     });
   }
 
   @override
   void initState() {
-    this.getVideoDetail();
+    (() async {
+      await this.getVideoInfo();
+      Future.delayed(Duration(milliseconds: 500), () {
+        this.getRecommendList();
+        this.getHotList();
+      });
+    })();
+
     super.initState();
   }
 
@@ -40,31 +71,69 @@ class _VideoDetailState extends State {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('视频详情'),
+        title: Text(videoInfo['vodName'] ?? ''),
       ),
-      body: Padding(
-        padding: EdgeInsets.all(16),
-        child: Scrollbar(
+      body: Column(
+        children: [
+          FPlayer(
+            key: Key(videoInfo['vodPlayUrl'].toString()),
+            data: videoInfo['vodPlayUrl'] ?? [],
+          ),
+          Expanded(
+              child: Scrollbar(
             controller: ScrollController(initialScrollOffset: 0),
             child: SingleChildScrollView(
-                child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                VideoInfo(data: videoDetail),
-                VideoCollection(
-                  title: '视频详情',
-                  data: videoDetail['vodPlayUrl'],
-                  handleClick: (index) {
-                    Navigator.pushNamed(context, 'videoPlayPage', arguments: {
-                      'collection': index,
-                      'videoId': routeArgs['videoId']
-                    });
-                  },
-                ),
-                VideoDesc(data: videoDetail['vodBlurb']),
-                VideoComment()
-              ],
-            ))),
+              scrollDirection: Axis.vertical,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        // 视频信息
+                        PlayVideoInfo(
+                          data: videoInfo,
+                        ),
+                        // 选集
+                        VideoCollection(
+                          key: Key(videoInfo['vodPlayUrl'].toString()),
+                          data: videoInfo['vodPlayUrl'] ?? [],
+                          title: '剧集与更新',
+                          handleClick: (int index) {
+                            print('集数$index');
+                            eventCenter['emit'](index);
+                          },
+                        ),
+                        FNav(title: '相似视频'),
+                        FHorizontalVideo(
+                          list: recommendList,
+                          jumpMethod: (arguments) => {
+                            Navigator.pushReplacementNamed(
+                                context, 'videoPlayPage',
+                                arguments: arguments)
+                          },
+                        ),
+                        FNav(title: '热播剧'),
+                        FHorizontalVideo(
+                          list: hotList,
+                          jumpMethod: (arguments) => {
+                            Navigator.pushReplacementNamed(
+                                context, 'videoPlayPage',
+                                arguments: arguments)
+                          },
+                        ),
+                        // 评论
+                        VideoComment()
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ))
+        ],
       ),
     );
   }
